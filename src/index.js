4496f6b55366bf1bbde9c004015b8003f17bb5af
sangjun <sgbo5003@naver.com>
2021-05-18 18:39:40 +0900

    index.js import 추가

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
-
 import MomentUtils from "@date-io/moment";
+import "moment/locale/ko";
+// import moment from "moment";
+
+// moment.locale("ru");
 
 ReactDOM.render(
   <MuiPickersUtilsProvider utils={MomentUtils}>
