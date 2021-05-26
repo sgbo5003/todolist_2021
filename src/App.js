95ecfcac4657b2b0e4252129b1226c0b256e5921
sangjun <sgbo5003@naver.com>
2021-05-26 11:13:05 +0900

    save 버튼 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { TextField, Typography } from "@material-ui/core";
+import { TextField, Typography, Button } from "@material-ui/core";
 import { KeyboardDatePicker, KeyboardTimePicker } from "@material-ui/pickers";
+import SaveIcon from "@material-ui/icons/Save";
 import React from "react";
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      todoList: [],
       title: "",
       content: "",
       startDate: null,
       startTime: null,
     };
+  }
+
+  saveTodoList() {
+    const { todoList, title, content, startDate, startTime } = this.state;
+    todoList.push({ title, content, startDate, startTime });
+    this.setState({
+      todoList,
+      title: "",
+      content: "",
+      startDate: null,
+      startTime: null,
+    });
   }
 
   render() {
@@ -25,6 +39,8 @@ class App extends React.Component {
             margin="normal"
             fullWidth
             required
+            value={this.state.title}
+            onChange={(e) => this.setState({ title: e.target.value })}
           />
           <TextField
             label="상세내용"
@@ -32,6 +48,8 @@ class App extends React.Component {
             margin="normal"
             fullWidth
             multiline
+            value={this.state.content}
+            onChange={(e) => this.setState({ title: e.target.value })}
           />
           <KeyboardDatePicker
             disableToolbarx
@@ -44,6 +62,10 @@ class App extends React.Component {
             KeyboardButtonProps={{
               "aria-label": "change date",
             }}
+            value={this.state.startDate}
+            onChange={(value) => {
+              this.setState({ startDate: value });
+            }}
           />
           <KeyboardTimePicker
             margin="normal"
@@ -54,7 +76,19 @@ class App extends React.Component {
             KeyboardButtonProps={{
               "aria-label": "change time",
             }}
+            value={this.state.startTime}
+            onChange={(value) => {
+              this.setState({ startTime: value });
+            }}
           />
+          <Button
+            variant="outlined"
+            startIcon={<SaveIcon />}
+            style={{ float: "right" }}
+            onClick={() => this.saveTodoList()}
+          >
+            Save
+          </Button>
         </div>
         <div className="list_area"> 리스트 영역</div>
         <Typography variant="body2" color="textSecondary" align="center">
