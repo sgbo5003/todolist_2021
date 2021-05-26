d3087f6baff953a57ba24b59ad71e3256ba41a5e
sangjun <sgbo5003@naver.com>
2021-05-26 11:57:27 +0900

    check Validate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { TextField, Typography, Button } from "@material-ui/core";
+import {
+  TextField,
+  Typography,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core";
 import { KeyboardDatePicker, KeyboardTimePicker } from "@material-ui/pickers";
 import SaveIcon from "@material-ui/icons/Save";
 import React from "react";
@@ -13,19 +20,51 @@ class App extends React.Component {
       content: "",
       startDate: null,
       startTime: null,
+      endDate: null,
+      endTime: null,
     };
   }
 
+  checkValidate() {
+    const { title, content, startDate, startTime, endDate, endTime } =
+      this.state;
+    if (!title || !content || !startDate || !endDate || !endTime) {
+      return false;
+    }
+    return true;
+  }
+
   saveTodoList() {
-    const { todoList, title, content, startDate, startTime } = this.state;
-    todoList.push({ title, content, startDate, startTime });
-    this.setState({
-      todoList,
-      title: "",
-      content: "",
-      startDate: null,
-      startTime: null,
-    });
+    if (this.checkValidate()) {
+      const {
+        todoList,
+        title,
+        content,
+        startDate,
+        startTime,
+        endDate,
+        endTime,
+      } = this.state;
+      todoList.push({
+        title: title.trim(),
+        content: content.trim(),
+        startDate,
+        startTime,
+        endDate,
+        endTime,
+      });
+      this.setState({
+        todoList,
+        // title: "",
+        // content: "",
+        // startDate: null,
+        // startTime: null,
+        // endDate: null,
+        // endTime: null,
+      });
+    } else {
+      alert("입력값을 확인해주세요");
+    }
   }
 
   render() {
@@ -81,6 +120,36 @@ class App extends React.Component {
               this.setState({ startTime: value });
             }}
           />
+          <KeyboardDatePicker
+            disableToolbarx
+            variant="inline"
+            format="yyyy/MM/DD"
+            margin="normal"
+            label="종료 예정일"
+            onChange={(value) => console.log(value)}
+            style={{ width: "50%" }}
+            KeyboardButtonProps={{
+              "aria-label": "change date",
+            }}
+            value={this.state.endDate}
+            onChange={(value) => {
+              this.setState({ startDate: value });
+            }}
+          />
+          <KeyboardTimePicker
+            margin="normal"
+            label="종료시간"
+            variant="inline"
+            onChange={(value) => console.log(value)}
+            style={{ width: "50%" }}
+            KeyboardButtonProps={{
+              "aria-label": "change time",
+            }}
+            value={this.state.endTime}
+            onChange={(value) => {
+              this.setState({ startTime: value });
+            }}
+          />
           <Button
             variant="outlined"
             startIcon={<SaveIcon />}
@@ -90,7 +159,30 @@ class App extends React.Component {
             Save
           </Button>
         </div>
-        <div className="list_area"> 리스트 영역</div>
+        <div className="list_area">
+          <List>
+            {this.state.todoList.map((todoItem, idx) => {
+              const { title, content, startDate, startTime, endDate, endTime } =
+                todoItem;
+              return (
+                <ListItem key={idx} role={undefined} dense button>
+                  <ListItemText
+                    primary={title}
+                    secondary={
+                      startDate?.format("yyyy-MM-DD") +
+                      " " +
+                      startTime?.format("HH:MM") +
+                      " ~ " +
+                      endDate?.format("yyyy-MM-DD") +
+                      " " +
+                      endTime?.format("HH:MM")
+                    }
+                  />
+                </ListItem>
+              );
+            })}
+          </List>
+        </div>
         <Typography variant="body2" color="textSecondary" align="center">
           {"Copyright ⓒ 박상준 " + new Date().getFullYear() + "."}
         </Typography>
